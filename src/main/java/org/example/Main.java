2d3da686f7ac7f05f7605d58d2711ae11ed469a8
endloc <li.amirova.l@gmail.com>
2023-01-05 02:16:36 +0500

    delete comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter array length: ");
-        int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
+        int size = input.nextInt();
         int numbers[] = new int[size];
         System.out.println("Insert array elements:");
-        /*Пройдёмся по всему массиву, заполняя его*/
+
         for (int i = 0; i < size; i++) {
-            numbers[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+            numbers[i] = input.nextInt();
         }
         java.util.Arrays.sort(numbers);
         System.out.println("Max element is " + numbers[size - 1]);
