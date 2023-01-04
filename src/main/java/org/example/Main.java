ec510c48b0cd76b3bfc116bf947995fff26792cf
endloc <li.amirova.l@gmail.com>
2023-01-05 02:06:41 +0500

    new: maximum search program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter array length: ");
+        int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
+        int numbers[] = new int[size];
+        System.out.println("Insert array elements:");
+        /*Пройдёмся по всему массиву, заполняя его*/
+        for (int i = 0; i < size; i++) {
+            numbers[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+        }
+        java.util.Arrays.sort(numbers);
+        System.out.println("Max element is " + numbers[size - 1]);
     }
 }
